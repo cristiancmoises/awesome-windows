e9388810f11e8ac16d19d2aabe30ab826289ab5b
Cristian Cezar Moisés <86272521+cristiancmoises@users.noreply.github.com>
2022-03-17 16:37:47 -0300

    Create awesomewindows.bat

---
+++ awesomewindows.bat
@@ -0,0 +1,251 @@
+:: Run as administrator
+wmic COMPUTERSYSTEM set AutomaticManagedPagefile=false
+wmic PAGEFILESET set InitialSize=10000,MaximumSize=16000
+
+@rem *** Disable Some Service ***
+sc stop DiagTrack
+sc stop diagnosticshub.standardcollector.service
+sc stop dmwappushservice
+sc stop WMPNetworkSvc
+sc stop WSearch
+
+sc config DiagTrack start= disabled
+sc config diagnosticshub.standardcollector.service start= disabled
+sc config dmwappushservice start= disabled
+REM sc config RemoteRegistry start= disabled
+REM sc config TrkWks start= disabled
+sc config WMPNetworkSvc start= disabled
+sc config WSearch start= disabled
+REM sc config SysMain start= disabled
+
+REM *** SCHEDULED TASKS tweaks ***
+REM schtasks /Change /TN "Microsoft\Windows\AppID\SmartScreenSpecific" /Disable
+schtasks /Change /TN "Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser" /Disable
+schtasks /Change /TN "Microsoft\Windows\Application Experience\ProgramDataUpdater" /Disable
+schtasks /Change /TN "Microsoft\Windows\Application Experience\StartupAppTask" /Disable
+schtasks /Change /TN "Microsoft\Windows\Customer Experience Improvement Program\Consolidator" /Disable
+schtasks /Change /TN "Microsoft\Windows\Customer Experience Improvement Program\KernelCeipTask" /Disable
+schtasks /Change /TN "Microsoft\Windows\Customer Experience Improvement Program\UsbCeip" /Disable
+schtasks /Change /TN "Microsoft\Windows\Customer Experience Improvement Program\Uploader" /Disable
+schtasks /Change /TN "Microsoft\Windows\Shell\FamilySafetyUpload" /Disable
+schtasks /Change /TN "Microsoft\Office\OfficeTelemetryAgentLogOn" /Disable
+schtasks /Change /TN "Microsoft\Office\OfficeTelemetryAgentFallBack" /Disable
+schtasks /Change /TN "Microsoft\Office\Office 15 Subscription Heartbeat" /Disable
+
+REM schtasks /Change /TN "Microsoft\Windows\Autochk\Proxy" /Disable
+REM schtasks /Change /TN "Microsoft\Windows\CloudExperienceHost\CreateObjectTask" /Disable
+REM schtasks /Change /TN "Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticDataCollector" /Disable
+REM schtasks /Change /TN "Microsoft\Windows\FileHistory\File History (maintenance mode)" /Disable
+REM schtasks /Change /TN "Microsoft\Windows\Maintenance\WinSAT" /Disable
+REM schtasks /Change /TN "Microsoft\Windows\NetTrace\GatherNetworkInfo" /Disable
+REM schtasks /Change /TN "Microsoft\Windows\PI\Sqm-Tasks" /Disable
+REM schtasks /Change /TN "Microsoft\Windows\Windows Error Reporting\QueueReporting" /Disable
+
+REM *** Remove Cortana ***
+REM Currently MS doesn't allow to uninstall Cortana using the above step claiming it's a required OS component (hah!)
+REM We will have to rename the Cortana App folder (add ".bak" to its name), but this can be done only if Cortana is not running.
+REM The issue is that when Cortana process (SearchUI) is killed, it respawns very quickly
+REM So the following code needs to be quick (and it is) so we can manage to rename the folder
+REM
+REM Disabling Cortana this way on Version 1703 (RS2) will render all items in the Start Menu unavailable.
+REM So this is commented out for now until a better solution is found.
+REM taskkill /F /IM SearchUI.exe
+REM move "%windir%\SystemApps\Microsoft.Windows.Cortana_cw5n1h2txyewy" "%windir%\SystemApps\Microsoft.Windows.Cortana_cw5n1h2txyewy.bak"
+
+@rem *** Remove Telemetry & Data Collection ***
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Device Metadata" /v PreventDeviceMetadataFromNetwork /t REG_DWORD /d 1 /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection" /v "AllowTelemetry" /t REG_DWORD /d 0 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\MRT" /v DontOfferThroughWUAU /t REG_DWORD /d 1 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\SQMClient\Windows" /v "CEIPEnable" /t REG_DWORD /d 0 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\AppCompat" /v "AITEnable" /t REG_DWORD /d 0 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\AppCompat" /v "DisableUAR" /t REG_DWORD /d 1 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DataCollection" /v "AllowTelemetry" /t REG_DWORD /d 0 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\WMI\AutoLogger\AutoLogger-Diagtrack-Listener" /v "Start" /t REG_DWORD /d 0 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\WMI\AutoLogger\SQMLogger" /v "Start" /t REG_DWORD /d 0 /f
+
+@REM Settings -> Privacy -> General -> Let apps use my advertising ID...
+reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo" /v Enabled /t REG_DWORD /d 0 /f
+REM - SmartScreen Filter for Store Apps: Disable
+reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\AppHost" /v EnableWebContentEvaluation /t REG_DWORD /d 0 /f
+REM - Let websites provide locally...
+reg add "HKCU\Control Panel\International\User Profile" /v HttpAcceptLanguageOptOut /t REG_DWORD /d 1 /f
+
+
+@REM *** Disable Cortana & Telemetry ***
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v "AllowCortana" /t REG_DWORD /d 0
+
+REM *** Disable MRU lists (jump lists) of XAML apps in Start Menu ***
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "Start_TrackDocs" /t REG_DWORD /d 0 /f
+
+REM *** Set Windows Explorer to start on This PC instead of Quick Access ***
+REM 1 = This PC, 2 = Quick access
+REM reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "LaunchTo" /t REG_DWORD /d 1 /f
+
+REM *** Disable Suggestions in the Start Menu ***
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v "SystemPaneSuggestionsEnabled" /t REG_DWORD /d 0 /f 
+
+@rem Remove Apps
+PowerShell -Command "Get-AppxPackage *3DBuilder* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Cortana* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Getstarted* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *bing* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *MicrosoftOfficeHub* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *OneNote* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *people* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *WindowsPhone* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *SkypeApp* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *solit* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *xbox* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *windowscommunicationsapps* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *zune* | Remove-AppxPackage"
+REM PowerShell -Command "Get-AppxPackage *WindowsMaps* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Sway* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *CommsPhone* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *ConnectivityStore* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Microsoft.Messaging* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *ContentDeliveryManager* | Remove-AppxPackage"
+
+@rem NOW JUST SOME TWEAKS
+REM *** Show file extensions in Explorer ***
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "HideFileExt" /t  REG_DWORD /d 0 /f
+
+REM *** Uninstall OneDrive ***
+start /wait "" "%SYSTEMROOT%\SYSWOW64\ONEDRIVESETUP.EXE" /UNINSTALL
+rd C:\OneDriveTemp /Q /S >NUL 2>&1
+rd "%USERPROFILE%\OneDrive" /Q /S >NUL 2>&1
+rd "%LOCALAPPDATA%\Microsoft\OneDrive" /Q /S >NUL 2>&1
+rd "%PROGRAMDATA%\Microsoft OneDrive" /Q /S >NUL 2>&1
+reg add "HKEY_CLASSES_ROOT\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}\ShellFolder" /f /v Attributes /t REG_DWORD /d 0 >NUL 2>&1
+reg add "HKEY_CLASSES_ROOT\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}\ShellFolder" /f /v Attributes /t REG_DWORD /d 0 >NUL 2>&1
+echo OneDrive has been removed. Windows Explorer needs to be restarted.
+pause
+start /wait TASKKILL /F /IM explorer.exe
+start explorer.exe
+
+pause
+
+REM *** Do Power Management Changes ***
+
+If /I "%hibernate_off%"=="y" (
+	ECHO.
+	ECHO Disabling Hibernation/Fast Boot
+	powercfg -h off
+	powercfg.exe -change -hibernate-timeout-dc 0
+	powercfg.exe -change -hibernate-timeout-ac 0
+)
+
+ECHO Setting the 'Power Management' to BEST PERFORMANCE
+powercfg -SETACTIVE 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
+
+ECHO Setting the unplugged settings to never hibernate
+powercfg.exe -change -monitor-timeout-dc 15
+powercfg.exe -change -standby-timeout-dc 0
+
+
+ECHO Setting the plugged in settings to never sleep
+powercfg.exe -change -monitor-timeout-ac 15
+powercfg.exe -change -standby-timeout-ac 0
+
+
+ECHO Setting the 'Dim Timeout' to Never
+powercfg -SETDCVALUEINDEX SCHEME_CURRENT 7516b95f-f776-4464-8c53-06167f40cc99 17aaa29b-8b43-4b94-aafe-35f64daaf1ee 0
+powercfg -SETACVALUEINDEX SCHEME_CURRENT 7516b95f-f776-4464-8c53-06167f40cc99 17aaa29b-8b43-4b94-aafe-35f64daaf1ee 0
+
+
+ECHO Set windows to do nothing when the lid is closed and it's plugged in, sleep when it's not
+powercfg -SETACVALUEINDEX SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0
+powercfg -SETDCVALUEINDEX SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 1
+
+
+ECHO Set windows to actually shut down when you press the power button, not just sleep
+powercfg -SETACVALUEINDEX SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 7648efa3-dd9c-4e3e-b566-50f929386280 3
+powercfg -SETDCVALUEINDEX SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 7648efa3-dd9c-4e3e-b566-50f929386280 3
+
+pause
+
+echo OFF
+
+NET SESSION >nul 2>&1
+
+IF %ERRORLEVEL% EQU 0 (
+
+   echo.
+
+) ELSE (
+
+   echo.-------------------------------------------------------------
+
+   echo ERROR: YOU ARE NOT RUNNING THIS WITH ADMINISTRATOR PRIVILEGES.
+
+   echo. -------------------------------------------------------------
+
+   echo. If you're seeing this, it means you don't have admin privileges!
+
+   pause
+
+   echo.
+
+   echo. You will need to restart this program with Administrator privileges by right-clicking and select "Run As Administrator"
+
+   pause 
+
+    echo.
+
+   echo Press any key to leave this program. Make sure to Run As Administrator next time!
+
+   pause
+
+   EXIT /B 1
+
+)
+
+powershell -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+choco feature enable -n=allowGlobalConfirmation
+
+echo Chocolatey is ready to begin installing packages!
+
+pause
+
+@rem ----[ ADD YOUR PROGRAMS BELOW! ] ----
+
+choco install winrar
+choco install 7zip.install
+choco install firefox
+choco install dotnetfx
+choco install flashplayerplugin
+choco install vlc
+choco install chocolatey-core.extension
+choco install anydesk.install
+choco install chocolatey-dotnetfx.extension
+choco install dotnet4.7
+choco install dotnet4.7.1
+choco install dotnet4.6
+choco install dotnet4.6.2
+choco install dotnet4.6.1
+choco install dotnet4.5
+choco install dotnet4.0
+choco install dotnet3.5
+choco install googlechrome
+choco install everything
+choco install flashplayeractivex
+choco install chocolatey-windowsupdate.extension
+choco install adobeshockwaveplayer
+choco install foxitreader
+choco install k-litecodecpackfull
+choco install flameshot
+choco install ccleaner
+choco install office365proplus
+choco install adobeair
+
+@rem ---[You can add as many programs here as you desire!]----
+
+pause
+echo.
+echo Your installation is complete.
+pause
+:: Run as administrator
+wmic COMPUTERSYSTEM set AutomaticManagedPagefile=false
+wmic PAGEFILESET set InitialSize=8000,MaximumSize=12000
+
+pause
